057e679a912fde85aab2d5f6366408b9b986aa5d
junaidumbc <mohd.junaid66666@gmail.com>
2023-06-12 11:56:57 -0400

    Logging and Exception

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 from typing import List
 
-HYPEN_E_DOT = "-e ."
+HYPEN_E_DOT = '-e .'
 def get_requirements(file_path : str)->List[str]:
     '''
     This Function will return the list of requirements
@@ -9,10 +9,11 @@ def get_requirements(file_path : str)->List[str]:
     requirements = []
     with open(file_path) as file_obj:
         requirements = file_obj.readlines()
-        requirements = [req.replace("\n", "") for req in requirements]
+        requirements = [req.replace("\n", " ") for req in requirements]
         
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
+    return requirements
 
 setup(
     name='machinelearningproject',
@@ -21,5 +22,5 @@ setup(
     author_email='mohd.junaid66666@gmail.com',
     packages=find_packages(),
     install_requires = get_requirements('requirements.txt')
-
 )
+
